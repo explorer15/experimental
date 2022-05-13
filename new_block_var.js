cc004c38f65f0a5824d09feadc0efa8d784fee79
explorer15 <105264825+explorer15@users.noreply.github.com>
2022-05-13 13:04:22 +0200

    Create new_block_var.js

---
+++ new_block_var.js
@@ -0,0 +1,17 @@
+<xml xmlns="http://www.w3.org/1999/xhtml">
+  <variables>
+    <variable type="" id="*km*zC)du=AFK{~_g.Jh" islocal="false" iscloud="false">HHHHHHHH</variable>
+  </variables>
+  <block type="event_whenflagclicked" id="`k4-f8-+!bz%8J|D/89a" x="343" y="304">
+    <next>
+      <block type="data_changevariableby" id="gq]3Nb9[WKrt/*x-2_JF">
+        <field name="VARIABLE" id="*km*zC)du=AFK{~_g.Jh" variabletype="">HHHHHHHH</field>
+        <value name="VALUE">
+          <shadow type="math_number" id="QQu+PgV^0,5zy,l31e~D">
+            <field name="NUM">658685547</field>
+          </shadow>
+        </value>
+      </block>
+    </next>
+  </block>
+</xml>
